64c1ebe000d8b65419a26afbc808522819228fee
Alessandro Bonomo <75626033+AlessandroBonomo28@users.noreply.github.com>
2023-05-19 22:55:18 +0200

    mansplaining content length

--- server/routing/handlers.c
+++ server/routing/handlers.c
@@ -29,15 +29,18 @@ void loginHandler(int client_socket, const char *body, const char *authorization
     int*a = 0;
     jsonFold(json,foldPrintKeyValueCount,&a);
     printf("val = %d",a);
-    char *response = "HTTP/1.1 401 Unauthorized\r\nContent-Length: 13\r\n\r\nNot Authorized!";
+    char *response = "HTTP/1.1 401 Unauthorized\r\nContent-Length: 15\r\n\r\nNot Authorized!";
     const char* desired_value = "alex";
-    const char* desired_key = "uer";
-    bool user = jsonCompare(json,desired_key,desired_value);
-    bool pw = jsonCompare(json,"password","1235");
+    const char* desired_key = "user";
+    bool user = jsonCompare(body,desired_key,desired_value);
+    bool pw = jsonCompare(body,"password","123");
     printf("pw %s\n",pw ? "true" : "false");
     printf("user %s\n",user ? "true" : "false");
     if(user && pw){
-        response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nAuthorized!";
+        // se la content length non matcha il count del body allora il browser
+        // non riesce a interpretare la richiesta.
+        // in questo caso 11 = len(Authorized!)
+        response = "HTTP/1.1 200 OK\r\nContent-Length: 11\r\n\r\nAuthorized!";
         printf("Authorized\n");
     }
     else printf("NOT Authorized\n");
