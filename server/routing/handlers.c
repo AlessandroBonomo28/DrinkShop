0bd5d0917781bf83b8e5ea059bf70b121a63acb4
Alessandro Bonomo <75626033+AlessandroBonomo28@users.noreply.github.com>
2023-05-20 15:58:55 +0200

    fix

--- server/routing/handlers.c
+++ server/routing/handlers.c
@@ -10,29 +10,13 @@ void homeHandler(int client_socket, const char *body, const char *authorization)
     const char *response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
     send(client_socket, response, strlen(response), 0);
 }
-void foldPrintKeyValueCount(const char* key, const char* value, void* accumulator) {
-    // Cast dell'accumulatore al tipo corretto (nel nostro caso, un puntatore a intero)
-    int* count = (int*)accumulator;
-
-    // Stampa la chiave e il valore
-    printf("Chiave: %s\n", key);
-    printf("Valore: %s\n", value);
-
-    // Incrementa il conteggio
-    (*count)++;
-}
 
 void loginHandler(int client_socket, const char *body, const char *authorization) {
     printf("Login Body: %s\n", body);
     const char* json = "{\"user\":\"alex\",\"password\":\"123\"}";
     printf("Login Authorization: %s\n", authorization);
-    int*a = 0;
-    jsonFold(json,foldPrintKeyValueCount,&a);
-    printf("val = %d",a);
     char *response = "HTTP/1.1 401 Unauthorized\r\nContent-Length: 15\r\n\r\nNot Authorized!";
-    const char* desired_value = "alex";
-    const char* desired_key = "user";
-    bool user = jsonCompare(body,desired_key,desired_value);
+    bool user = jsonCompare(body,"user","alex");
     bool pw = jsonCompare(body,"password","123");
     printf("pw %s\n",pw ? "true" : "false");
     printf("user %s\n",user ? "true" : "false");
