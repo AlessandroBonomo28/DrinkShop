25a3f8527344345a7a064f072bd253ff0cbc9a4a
Alessandro Bonomo <75626033+AlessandroBonomo28@users.noreply.github.com>
2023-06-21 16:14:53 +0200

    Update server.h

--- server/server.h
+++ server/server.h
@@ -2,10 +2,10 @@
 #define SERVER_H
 #include <postgresql/libpq-fe.h>
 // PRODUCTION CONFIGURATION 
-//#define DATABASE "host=postgres-db port=5432 dbname=drinks user=docker password=12345"
+#define DATABASE "host=postgres-db port=5432 dbname=drinks user=docker password=12345"
 
 // DEBUG CONFIGURATION 
-#define DATABASE "host=127.0.0.1 port=42069 dbname=drinks user=docker password=12345"
+//#define DATABASE "host=127.0.0.1 port=42069 dbname=drinks user=docker password=12345"
 #define PORT 4040
 #define MAX_CLIENTS 100
 #define TIMEOUT_SECONDS_ABORT_CONNECTION 5
