df6df1b82acfcdab9712120d48379ed0a3cbf4e4
Mario De Luca <bloodcreed21@gmail.com>
2023-06-27 17:35:24 +0200

    login screen

--- client/app/src/main/java/com/uninaproject/juicylemon/MainActivity.java
+++ client/app/src/main/java/com/uninaproject/juicylemon/MainActivity.java
@@ -1,14 +1,20 @@
 package com.uninaproject.juicylemon;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_register);
+
+        Button btnLogin = findViewById(R.id.register_login_button);
+
+        btnLogin.setOnClickListener( v -> setContentView(R.layout.activity_main));
     }
+
+
 }
